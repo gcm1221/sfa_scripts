d4f4b4fc94422bf59bc194d471b4b779cb5cb0b7
gcm1221 <69770787+gcm1221@users.noreply.github.com>
2020-09-21 16:42:04 -0500

    Update scenefile.py

--- src/scenefile.py
+++ src/scenefile.py
@@ -2,15 +2,16 @@ from pathlib import Path
 
 class SceneFile(object):
     """An abstract representation of a Scene file."""
-    def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = Path(folder_path)
-        self.descriptor = descriptor
-        self.task = task
-        self. ver = ver
-        self.ext = ext
+    def __init__(self, path):
+        self.folder_path = Path()
+        self.descriptor = 'main'
+        self.task = None
+        self.ver = 1
+        self.ext = '.ma'
+        self._init_from_path(path)
 
     @property
-    def filename (self):
+    def filename(self):
         pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
         return pattern.format(descriptor=self.descriptor,
                               task=self.task,
@@ -21,6 +22,14 @@ class SceneFile(object):
     def path(self):
         return self.folder_path / self.filename
 
+    def _init_from_path(self, path):
+        path = Path(path)
+        self.folder_path = path.parent
+        self.ext = path.suffix
+        self.descriptor, self.task, ver = path.stem.split("_")
+        self.ver = int(ver.split("v")[-1])
 
-scene_file = SceneFile("D:/", "tank", "model", 1, ".ma")
+
+scene_file = SceneFile("D:/tank_model_v001.ma")
 print(scene_file.path)
+print(scene_file.filename)
