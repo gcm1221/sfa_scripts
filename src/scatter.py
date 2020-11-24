70ac490078781dbe9b45d30aa351012599b9aa7a
gcm1221 <69770787+gcm1221@users.noreply.github.com>
2020-11-23 20:43:00 -0600

    Update scatter.py

--- src/scatter.py
+++ src/scatter.py
@@ -1,26 +1,42 @@
-{
-     string $selection[] = `ls -os -fl`;
+#randominstances.py
 
-    string $vertexNames[] = `filterExpand -selectionMask 31 -expand true $selection`;
+import maya.cmds as cmds
+import random
 
-     //print $vertexNames;
-     
-     string $objectToInstance = $selection[0];
-     
-     if( `objectType $objectToInstance` == "transform" ) {
-         
-         string $vertex;
-         for( $vertex in $vertexNames ) {
-             
-             string $newInstance[] = `instance $objectToInstance`;
-             
-             vector $position = `pointPosition -w $vertex`;
-             
-             move -a -ws ($position.x) ($position.y) ($position.z) $newInstance;
-         }
-         
-     } else {
-         
-         print "Please ensure the first object  you select is a transform.";
-     }
-}
+random.seed( 1234 )
+
+result = cmds.ls(orderedSelection=True )
+
+print 'result:%s' % str(result)
+
+transformName = result[0]
+
+instanceGroupName = cmds.group(empty=True, name=transformName + '_instance_grp#' )
+
+for i in range ( 0, 50 ):
+    
+    instanceResult = cmds.instance (transformName, name=transformName + '_instance#')
+    
+    cmds.parent( instanceResult, instanceGroupName)
+    
+#    print 'instanceResult: ' + str ( instanceResult )
+    
+    x = random.uniform( -10, 10)
+    y = random.uniform ( 0, 20)
+    z = random.uniform ( -10, 10)
+
+    cmds.move (x, y, z, instanceResult )
+    
+    xRot = random.uniform(0, 360)
+    yRot = random.uniform(0, 360)
+    zRot = random.uniform(0, 360)
+
+    cmds.rotate(xRot, yRot, zRot, instanceResult )
+
+    scalingFactor = random.uniform( 0.3,1.5 ) 
+
+    cmds.scale( scalingFactor, scalingFactor, scalingFactor, instanceResult)
+    
+    cmds.hide(transformName)
+    
+    cmds.xform( instanceGroupName, centerPivots=True ) 
